9bfcc041a9b60808c0888fb7df2ddbcd0b0b3aee
Loris Cro <kappaloris@gmail.com>
2024-06-21 18:20:38 +0200

    add diffing tooling from zls

--- build.zig
+++ build.zig
@@ -4,9 +4,14 @@ pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const diffz = b.dependency("diffz", .{});
+
     _ = b.addModule("lsp", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
+        .imports = &.{
+            .{ .name = "diffz", .module = diffz.module("diffz") },
+        },
     });
 }
--- src/root.zig
+++ src/root.zig
@@ -2,3 +2,4 @@ pub const types = @import("lsp.zig");
 pub const offsets = @import("offsets.zig");
 pub const server = @import("server.zig");
 pub const Transport = @import("Transport.zig");
+pub const diff = @import("diff.zig");
